36377b8e51988740411bc4973bf8456338af6982
Michal Nowicki <michalsminowicki@gmail.com>
2017-04-11 11:10:18 +0200

    Correcting auto instruction

--- freeglut/gameAuto/modelview.cpp
+++ freeglut/gameAuto/modelview.cpp
@@ -1,13 +1,11 @@
-// Zapisanie aktualnej macierzy
+﻿// Zapisanie aktualnej macierzy
 glPushMatrix();
 
 // Przesunięcie auta według ostatniej pozycji
-glTranslated(auto->translacja[0], auto->translacja[1], auto->translacja[2]);
+glTranslated(auto->translacja[0], auto->translacja[1], 0.0);
 
 // Obrót auta według ostatniej pozycji
-glRotated(auto->rotacja[0], 1.0, 0.0, 0.0);
-glRotated(auto->rotacja[1], 0.0, 1.0, 0.0);
-glRotated(auto->rotacja[2], 0.0, 0.0, 1.0);
+glRotated(auto->rotacja, 1.0, 0.0, 0.0);
 
 // Wykonanie ruchu do przodu w układzie auta
 glTranslated(0.0, 0.1, 0.0);
@@ -21,7 +19,7 @@ double modelViewMatrix[16];
 glGetDoublev(GL_MODELVIEW_MATRIX, modelViewMatrix);
 
 // Ustawienie aktualnej pozycji auta (elementy liczone kolumnami)
-auto->UstawPozycje(modelViewMatrix[12], modelViewMatrix[13], 0.0);
+auto->ustawPozycje(modelViewMatrix[12], modelViewMatrix[13], 0.0);
 
 // Powrót do układu początkowego
 glPopMatrix();
---
+++ freeglut/gameAuto/modelviewEN.cpp
@@ -0,0 +1,25 @@
+﻿// Save the current matrix
+glPushMatrix();
+
+// Move the auto to the last position
+glTranslated(auto->translation[0], auto->translation[1], 0.0);
+
+// Rotate the auto according to the last rotation
+glRotated(auto->rotation, 0.0, 0.0, 1.0);
+
+// Move the car forward
+glTranslated(0.0, 0.1, 0.0);
+
+// Get the current position as translation and rotation:
+// 		/ r11 r12 r13 t1 \
+// 		| r21 r22 r23 t2 |
+// 		| r31 r32 r33 t3 |
+// 		\ 0    0     0   1 /
+double modelViewMatrix[16];
+glGetDoublev(GL_MODELVIEW_MATRIX, modelViewMatrix);
+
+// Set the current position of the car (numbering by columns)
+auto->setPosition(modelViewMatrix[12], modelViewMatrix[13], 0.0);
+
+// We go back to the original coordinate system 
+glPopMatrix();
